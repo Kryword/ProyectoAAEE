990f1f88dc102e95cde04e201eea204d6127d2d9
Kryword <kryword@gmail.com>
2017-12-16 22:50:21 +0100

    Terminada la primera versión de la aplición. Recoge la foto y traduce.

--- src/main/java/aaee/HelloAppEngine.java
+++ src/main/java/aaee/HelloAppEngine.java
@@ -2,7 +2,9 @@ package aaee;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Base64;
 
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +18,19 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 
 import com.google.protobuf.ByteString;
+import com.google.appengine.repackaged.com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
 
 @SuppressWarnings("serial")
 @WebServlet(
@@ -38,14 +53,16 @@ public class HelloAppEngine extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) 
 	      throws IOException {
-	  response.setContentType("text/html");
+	  response.setContentType("text/html; charset=UTF-8");
 	  response.setCharacterEncoding("UTF-8");
-	  response.getOutputStream().print("<!DOCTYPE html>\n" + 
+	  PrintWriter out = response.getWriter();
+	  GoogleCredential credential = GoogleCredential.getApplicationDefault();
+	  out.print("<!DOCTYPE html>\n" + 
 	  		"<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\">\n" + 
 	  		"\n" + 
 	  		"  	<head>\n" + 
 	  		"	    <link rel=\"stylesheet\" href=\"css/estilos.css\">\n" + 
-	  		"	    <meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />\n" + 
+	  		"	    <meta charset=\"UTF-8\">\n" + 
 	  		"	    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + 
 	  		"	    <title>ProyectoAAEE</title>\n" + 
 	  		"  	</head>\n" + 
@@ -53,43 +70,88 @@ public class HelloAppEngine extends HttpServlet {
 	  		"  	<body>");
 	  try {
 		  List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+		  String lang = "en"; //English by default
+		  String fileName = "";
+		  String fileType = "";
+		  InputStream fileContent = null;
+		  long fileSizeLong = 0;
 	        for (FileItem item : items) {
 	            if (item.isFormField()) {
 	                // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
 	                String fieldName = item.getFieldName();
 	                String fieldValue = item.getString();
-	                response.getOutputStream().print("<div><h3 class=\"blockTitle\">Parsing field</h3><ul>"
-	                		+ "<li>Field Name:" + fieldName + "\n"
-            				+ "<li>Field Value:" + fieldValue + "\n</ul>"
-    						+ "</div>");
+	                if (fieldName == "lang" && !fieldValue.isEmpty()) {
+	                	lang = fieldValue;
+	                }
 	            } else {
-	                // Process form file field (input type="file").
+	                // Proceso el campo del fichero, de aquí obtengo la imágen que proceso después
 	                String fieldName = item.getFieldName();
-	                String fileName = FilenameUtils.getName(item.getName());
-	                String fileSize = Long.toString(item.getSize());
-	                String fieldType = item.getContentType();
-	                InputStream fileContent = item.getInputStream();
-	                ByteString byteImg = ByteString.readFrom(fileContent);
-	                byte[] img = Base64.getEncoder().encode(byteImg.toByteArray());
-	                /* Muestro datos del fichero */
-	                response.getOutputStream().print("<div><h3 class=\"blockTitle\">Parsing file</h3><ul>"
-	                		+ "<li>Field Name:" + fieldName + "\n"
-            				+ "<li>File Name:" + fileName + "\n"
-    						+ "<li>File Size:" + fileSize + "\n"
-							+ "<li>File Type:" + fieldType + "</ul>\n"
-							+ "<h2>--- Imagen ---</h2>\n"
-							+ "<img src=\"data:image/png;base64, ");
-						
-	                /* Muestro la imágen */
-	                response.getOutputStream().write(img);
+	                fileName = FilenameUtils.getName(item.getName());
+	                fileSizeLong = item.getSize();
+	                fileType = item.getContentType();
 	                
-	                /* Muestro el final del div */
-					response.getOutputStream().print("\" alt=\"No image found\"/></div>");
+	                fileContent = item.getInputStream();
+	                
 	            }
 	        }
+	        
+	        //Procesamiento del fichero
+	        /* Muestro datos del fichero */
+	        if(fileSizeLong > 3000000) {
+            	out.print("<h2>Imágen introducida supera los 3MB permitidos. Introduzca una imágen de menor tamaño.</h2></div>");
+            	out.print("</body></html>");
+            	return;
+            }
+	        if(fileSizeLong == 0) {
+            	out.print("<h2>Error: No ha introducido ningún archivo.</h2></div>");
+            	out.print("</body></html>");
+            	return;
+            }
+	        ByteString byteImg = ByteString.readFrom(fileContent);
+            out.print("<div>"
+					+ "<h2>--- Imagen ---</h2>\n"
+					+ "<img src=\"data:image/png;base64, ");
+				
+            /* Muestro la imágen */
+            out.print(Base64.getEncoder().encodeToString(byteImg.toByteArray()));
+            
+            /* Muestro el final del div */
+			out.print("\" alt=\"No image found\"/>");
+			
+			// Detección de texto dentro de la imágen
+			ImageAnnotatorClient vision = ImageAnnotatorClient.create();
+		    
+		    List<AnnotateImageRequest> requests = new ArrayList<>();
+		    Image sendImage = Image.newBuilder().setContent(byteImg).build();
+		    Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+		    Feature featText = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+		    AnnotateImageRequest requestForVision = AnnotateImageRequest.newBuilder().addFeatures(feat).addFeatures(featText).setImage(sendImage).build();
+		    requests.add(requestForVision);
+		    //Envío la request y recojo la respuesta en responseVision
+		    BatchAnnotateImagesResponse responseVision = vision.batchAnnotateImages(requests);
+		    List<AnnotateImageResponse> responses = responseVision.getResponsesList();
+		    String textOCR = "";
+		    //Recorro todas las respuestas recibidas
+		    for (AnnotateImageResponse res: responses) {
+			  if (res.hasError()) {
+				    System.out.printf("Error: %s\n", res.getError().getMessage());
+					return;	
+			  }
+			  out.print("<h3>Lista de labels</h3><ul>");
+			  for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+				  out.print("<li>" + annotation.getDescription() + "\n");
+			  }
+			  textOCR = res.getFullTextAnnotation().getText();
+			  out.print("</ul><h3>Full Detection of Text</h3>\n<p id=\"textOCR\">" + textOCR + "</p>");
+		    }
+		    Translate translate = TranslateOptions.getDefaultInstance().getService();
+		    Translation translation = translate.translate(textOCR, TranslateOption.targetLanguage(lang));
+		    out.print("<h3>Translation of text to desired lenguage</h3><p>" + translation.getTranslatedText() + "</p>");
+		    out.print("</div>");
+		    //System.out.println("Texto Traducido:" + translation.getTranslatedText());
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
-	response.getOutputStream().print("</body></html>");
+	out.print("</body></html>");
   }
 }
